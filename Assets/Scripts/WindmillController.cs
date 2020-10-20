4b74d219ac4b2be8c3a58f43642ae62dc6a2af4c
ceily green <avocado5001@gmail.com>
2020-10-20 19:53:20 -0400

    Added windmill object

---
+++ Assets/Scripts/WindmillController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindmillController : MonoBehaviour
+{
+
+   public enum Axis {X, Y, Z};
+   public Axis rotateAxis;
+   public float rotateSpeed;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      float xAmount = 0f;
+      float yAmount = 0f;
+      float zAmount = 0f;
+      if (rotateAxis == Axis.X)
+      {
+         xAmount = rotateSpeed;
+      }
+      else if (rotateAxis == Axis.Y)
+      {
+         yAmount = rotateSpeed;
+      }
+      else
+      {
+         zAmount = rotateSpeed;
+      }
+      transform.Rotate(xAmount * Time.deltaTime, yAmount * Time.deltaTime, zAmount * Time.deltaTime);
+    }
+}
